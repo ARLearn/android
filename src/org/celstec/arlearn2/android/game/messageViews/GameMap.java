826a3c83511cb804216f785eecbb3861f4e16cf0
Stefaan Ternier <stefaan.ternier@gmail.com>
2014-10-03 14:03:27 +0200

    add code for map view and swiping between general items

--- src/org/celstec/arlearn2/android/game/messageViews/GameMap.java
+++ src/org/celstec/arlearn2/android/game/messageViews/GameMap.java
@@ -2,9 +2,11 @@ package org.celstec.arlearn2.android.game.messageViews;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.*;
 import org.celstec.arlearn2.android.R;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.ResourceProxyImpl;
+import org.osmdroid.views.MapView;
 
 /**
  * ****************************************************************************
@@ -33,11 +35,24 @@ public class GameMap extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.ARLearn_schema2);
-        setContentView(R.layout.game_general_item);
+        setContentView(R.layout.game_mapview);
 
         gameActivityFeatures = new GameActivityFeatures(this);
         actionBarMenuController = new ActionBarMenuController(this, gameActivityFeatures);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.getController().setZoom(5);
     }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+//    {
+//        ResourceProxyImpl mResourceProxy = new ResourceProxyImpl(inflater.getContext().getApplicationContext());
+//        MapView mMapView = new MapView(inflater.getContext(), 256, mResourceProxy);
+//        return mMapView;
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
